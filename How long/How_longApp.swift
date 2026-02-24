1eccec05ae783341ea10e372e43cb5ce01c4b7f4
Basile Bruneau <basile@bruneau.email>
2026-02-24 23:03:57 +0100

    Initial Commit

---
+++ How long/How_longApp.swift
@@ -0,0 +1,17 @@
+//
+//  How_longApp.swift
+//  How long
+//
+//  Created by Basile Bruneau on 24/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct How_longApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
